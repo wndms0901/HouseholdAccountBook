44d2c87c55f891e981f20c0f8568bf62517471f0
wndms0901 <cherry25987@gmail.com>
2021-03-29 15:25:15 +0900

    20210329 프로젝트 생성

---
+++ src/main/java/com/app/HouseholdAccountBookApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseholdAccountBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseholdAccountBookApplication.class, args);
+	}
+
+}
