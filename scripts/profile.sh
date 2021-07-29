4e8f006160a86443beb1b8bcf9345e3b220f7bbd
wndms0901 <cherry25987@gmail.com>
2021-07-29 21:54:11 +0900

    210729 URL 변수 추가

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+URL=$(</etc/nginx/conf.d/service-url.inc)
+echo "$URL"
+
 # bash는 return value가 안되니 *제일 마지막줄에 echo로 해서 결과 출력*후, 클라이언트에서 값을 사용한다
 
 # 쉬고 있는 profile 찾기: real1이 사용중이면 real2가 쉬고 있고, 반대면 real1이 쉬고 있음
@@ -23,6 +26,7 @@ function find_idle_profile()
     fi
 
     echo "> RESPONSE_CODE: ${RESPONSE_CODE}"
+    echo ">service-url: ${service-url}"
     #echo "${IDLE_PROFILE}"
 }
 
